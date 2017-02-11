240dbfef46163f51ad5c58566f9765dfca4d59fc
Elbek Azimov <atom.azimov@gmail.com>
2017-02-11 17:51:45 +0300

    GERONIMO !!!

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { ComprateFrontendPage } from './app.po';
+
+describe('comprate-frontend App', function() {
+  let page: ComprateFrontendPage;
+
+  beforeEach(() => {
+    page = new ComprateFrontendPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
